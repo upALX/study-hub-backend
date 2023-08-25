fcfae97c1dd611606c7d0b76b10f0bb7a4a45cf7
root <root@ALX.localdomain>
2023-08-25 16:02:39 -0300

    adding validation phone number and mask number to project lib

--- data_validators/src/modules/phone_number_validator/phone_number_validator.py
+++ data_validators/src/modules/phone_number_validator/phone_number_validator.py
@@ -20,5 +20,24 @@ class PhoneNumberValidator:
         else:
             return False
 
-    def get_number(self) -> str:
-        return self.number
+    def get_number_formated(self, phone_number: str) -> str:
+        return self.format_number(phone_number=phone_number)
+
+    def format_number(self, phone_number: str) -> str:
+        phone_number_validated = self.validate_phone_number(phone_number=phone_number)
+
+        print(phone_number_validated)
+
+        if phone_number_validated is True:
+            pattern_regex_phone_number = '([0-9]{2,3})([0-9]{2})([0-9]{9})'
+            phone_number_founded = re.search(pattern=pattern_regex_phone_number, string=phone_number)
+            print('The phone number founded is ', phone_number_founded)
+            formated_phone_number = f'+{phone_number_founded.group(1)} ({phone_number_founded.group(2)}) {phone_number_founded.group(3)[0:5]}-{phone_number_founded.group(3)[5:9]}'
+
+            print('The phone number formated is:', formated_phone_number)
+
+            return  formated_phone_number
+        else:
+            raise Exception('Phone number was invalid!')
+        
+        
--- data_validators/tests/modules/phone_validator/test_phone_validator.py
+++ data_validators/tests/modules/phone_validator/test_phone_validator.py
@@ -10,3 +10,10 @@ class TestPhoneValidator:
         phone_number_validated = self.phone_number_validator.validate_phone_number(phone_number='5551980115295')
 
         assert phone_number_validated == True
+
+    def test_get_formated_phone_number_by_number(self): 
+        phone_number_formated = self.phone_number_validator.get_number_formated(
+            phone_number='5551980115295'
+        )
+
+        assert phone_number_formated == '+55 (51) 98011-5295'
