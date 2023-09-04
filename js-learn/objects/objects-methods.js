03c10a770d050d21bbf028de8f130c7df797de83
Alexandre <alexandreaddres@gmail.com>
2023-09-04 20:27:21 -0300

    adding objects manipulation into the project

--- js-learn/objects/objects-methods.js
+++ js-learn/objects/objects-methods.js
@@ -1,9 +1,23 @@
 //create a object with some items 
-
+const alx =  {
+    name: "Alexandre",
+    age: 23,
+    gender: 'male',
+    locationNumber: '77777-777' 
+}
 
 // create a object with same properties of the object created before.
+const alxClone = Object.create(alx)
 
+alxClone.name = 'ALX CLONE'
+alxClone.locationNumber = '66666-666'
+alxClone.age = 23
+alxClone.gender = 'Male'
+
+console.log(alxClone)
 
 // pass over all items of object and show these
 
-
+Object.keys(alxClone).forEach(keys => {
+    console.log(`${keys}: ${alxClone[keys]}`)
+});
