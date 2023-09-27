039e35a11de736f953360953015c374aaf755251
Alexandre <alexandreaddres@gmail.com>
2023-09-27 08:57:20 -0300

    adding learn heritage and creating objects instance with javascript

--- js-learn/oop-js/prototype/prototype-heritage.js
+++ js-learn/oop-js/prototype/prototype-heritage.js
@@ -1,13 +1,51 @@
+const util = require('util')
 
 //Use of THIS
 const alx = {
     "name": 'alx',
     "age": 14,
     "email": 'alexandreaddres@gmail.com',
-    showValues: () => {
+    showValues: function() {
         console.log(`name: ${this.name}, age: ${this.age}, email: ${this.email}`)
     }
 }
 
 alx.showValues()
 
+//Creating object with schema of other proto - Heritage
+const alxJunior = {
+    "name": 'Alexandre',
+    "age": 23,
+    "email": 'alexandreaddres@gmail.com',
+    show: function() {
+        console.log('Hello world!')
+    }
+}
+
+Object.setPrototypeOf(alxJunior, alx)
+alxJunior.show()
+alxJunior.showValues()
+
+//Creatig objects with javascript  = new vs create
+
+function Person(name, age, email){
+    this.name = name
+    this.age = age
+    this.email = email
+}
+
+const a = new Person('Alexandre', 23, 'a@a')
+
+console.log(a)
+
+//Creating object with literal object
+
+const l = {name: 'ALX', age: 17}
+
+const x = Object.create(l)
+
+x.name = 'OLa'
+x.age = 18
+
+console.log(l)
+console.log(x)
