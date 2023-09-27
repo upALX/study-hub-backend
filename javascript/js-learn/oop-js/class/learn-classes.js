d0d33676d5dbc35c779f8e3f846b841c74a6e7cc
Alexandre <alexandreaddres@gmail.com>
2023-09-27 11:36:20 -0300

    adding classes learn to project

--- javascript/js-learn/oop-js/class/learn-classes.js
+++ javascript/js-learn/oop-js/class/learn-classes.js
@@ -21,3 +21,20 @@ const alx = new User('Alexandre', 23, 'alexandreaddres@gmail.com')
 
 console.log(alx)
 console.log(alx.getUserInformation())
+
+// Use heritage class in JS
+
+class Person extends User{
+    constructor(name, age, email, address){
+        super(name, age, email)
+        this.address = address
+    }
+
+    getPersonInformation(){
+        console.log(this.name, this.age, this.address)
+    }
+}
+
+const newPerson = new Person('ALX', 19, 'ALX@ALX.COM', {street: 'Firmina'})
+
+console.log(newPerson.getPersonInformation())
