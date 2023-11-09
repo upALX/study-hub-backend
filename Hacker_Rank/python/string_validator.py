1f19cb6e67abd9d3d15205381ade42a3734c7eae
Alexandre <alexandreaddres@gmail.com>
2023-11-09 10:41:57 -0300

    fix solution string validation hacker rank

--- Hacker_Rank/python/string_validator.py
+++ Hacker_Rank/python/string_validator.py
@@ -2,15 +2,18 @@
 # Your task is to find out if the string  contains: alphanumeric characters, alphabetical characters, digits, lowercase and uppercase characters.
 
 def check_string(string):
-    list_bol_verify = []
+    
 
-    for value in string.split():
+    #Because some functions returns True to values WHITESPACE
+    new_string = string.replace(" ", "")
 
-        list_bol_verify.append(value.isalnum())
-        list_bol_verify.append(value.isalpha())
-        list_bol_verify.append(value.isdigit())
-        list_bol_verify.append(value.islower())
-        list_bol_verify.append(value.isupper())
+    isalnum = any(value.isalnum() for value in new_string)
+    isalpha = any(value.isalpha() for value in new_string)
+    isdigit = any(value.isdigit() for value in new_string)
+    islower = any(value.islower() for value in new_string)
+    isupper = any(value.isupper() for value in new_string)
+
+    list_bol_verify = [isalnum, isalpha, isdigit, islower, isupper] 
 
     return  list_bol_verify
 
