0c71959f0451253a2bd1dcac449d95602c58d577
Alexandre <alexandreaddres@gmail.com>
2023-11-11 16:19:58 -0300

    adding exercise re python done

---
+++ Hacker_Rank/python/minion_game.py
@@ -0,0 +1,18 @@
+# Game Rules
+
+# Both players have to make substrings using the letters of the string .
+# Stuart has to make words starting with consonants.
+# Kevin has to make words starting with vowels.
+# The game ends when both players have made all possible substrings.
+
+# Scoring
+# A player gets +1 point for each occurrence of the substring in the string .
+
+# For Example:
+# String  = BANANA
+# Kevin's vowel beginning word = ANA
+# Here, ANA occurs twice in BANANA. Hence, Kevin will get 2 Points.
+
+
+# Both players are given the same string, .
+player01
---
+++ Hacker_Rank/python/python_regex.py
@@ -0,0 +1,14 @@
+# You are given a string  consisting only of digits 0-9, commas ,, and dots .
+
+# Your task is to complete the regex_pattern defined below, which will be used to re.split() all of the , and . symbols in .
+
+# It’s guaranteed that every comma and every dot in  is preceeded and followed by a digit.
+
+import re
+
+
+regex_pattern = r"(?<=[0-9])[.,](?=[0-9])"	# Do not delete 'r'.
+
+
+
+print("\n".join(re.split(regex_pattern, input())))
