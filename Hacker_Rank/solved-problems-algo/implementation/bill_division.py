509ac570bdbcbcc5b9a6dd5a27016b0700b63777
Alexandre <alexandreaddres@gmail.com>
2023-11-24 04:14:29 -0300

    bill division - solved - calculating the fair value to pay for dinner

---
+++ Hacker_Rank/solved-problems-algo/implementation/bill_division.py
@@ -0,0 +1,44 @@
+# Two friends Anna and Brian, are deciding how to split the bill at a dinner. Each will only pay for the items they consume. Brian gets the check and calculates Anna's portion. You must determine if his calculation is correct.
+
+# For example, assume the bill has the following prices: . Anna declines to eat item  which costs . If Brian calculates the bill correctly, Anna will pay . If he includes the cost of , he will calculate . In the second case, he should refund  to Anna.
+
+
+# sum(bill) 
+# s - bill[k] / 2 == b
+# if b - 
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'bonAppetit' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY bill
+#  2. INTEGER k
+#  3. INTEGER b
+#
+
+def bonAppetit(bill, k, b):
+    sum_bill = sum(bill)
+    if (sum_bill - bill[k]) / 2 == b:
+        print('Bon Appetit')
+    else:
+        print(int(b - ((sum_bill - bill[k]) / 2)))
+
+first_multiple_input = input().rstrip().split()
+
+n = int(first_multiple_input[0])
+
+k = int(first_multiple_input[1])
+
+bill = list(map(int, input().rstrip().split()))
+
+b = int(input().strip())
+
+bonAppetit(bill, k, b)
