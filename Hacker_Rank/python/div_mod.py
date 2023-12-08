f91ee856187e0af4d49756ac52e45cd516d31f20
Alexandre <alexandreaddres@gmail.com>
2023-12-07 23:02:15 -0300

    Hacker Rank div mod python solution

---
+++ Hacker_Rank/python/div_mod.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+a = int(input())
+b = int(input())
+
+div_mod_of_value = divmod(a,b)
+print(div_mod_of_value[0])
+print(div_mod_of_value[1])
+print(div_mod_of_value)
