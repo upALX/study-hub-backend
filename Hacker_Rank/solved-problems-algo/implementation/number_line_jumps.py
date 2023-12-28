914e2545ab81d96e43295eb14fb61d78491e5680
Alexandre <alexandreaddres@gmail.com>
2023-12-28 11:15:01 -0300

    adding number line jumps - done

--- Hacker_Rank/solved-problems-algo/implementation/number_line_jumps.py
+++ Hacker_Rank/solved-problems-algo/implementation/number_line_jumps.py
@@ -1,18 +1,54 @@
+#!/bin/python3
+
+#
+# Complete the 'kangaroo' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER x1
+#  2. INTEGER v1
+#  3. INTEGER x2
+#  4. INTEGER v2
+#
+
+# x1 + v1 compair with x2 + v2 
+# if in one of jumps both camgus are in the same position
+
 def kangaroo(x1, v1, x2, v2):
-    # Write your code here
-
-    #x1 is start location of jumps
-
-    #v1 is the lenght of each jump
-
-    #x2 is start location of kangaroo 2 jumps
-
-    #v2 is the lenght of each jump
-
-    # find a form to set both kangoroos on the same position on show. If is possible, print YES, if not print NO
-
-   pass
-
+    
+    counter = 0
+    camgu_position_1 = 0
+    camgu_position_2 = 0
+    
+    while range(0, 10000):
+        if counter == 0:
+            if x1 == 0:
+                camgu_position_1 += v1
+            else:
+                camgu_position_1 += x1 + v1
+        else:
+            camgu_position_1 += v1
+        
+        if counter == 0:  
+            if x2 == 0:
+                camgu_position_2 += v2
+            else:
+                camgu_position_2 += x2 + v2
+        else:
+            camgu_position_2 += v2
+        
+        # print('CAMGU POSITION A', camgu_position_1)
+        # print('CAMGU POSITION B', camgu_position_2)
+        
+        if camgu_position_1 == camgu_position_2:
+            print('YES')
+            break
+        
+        counter += 1
+        
+        if counter == 10000:
+            print('NO')
+            break
 
 first_multiple_input = input().rstrip().split()
 
@@ -24,4 +60,4 @@ x2 = int(first_multiple_input[2])
 
 v2 = int(first_multiple_input[3])
 
-result = kangaroo(x1, v1, x2, v2)
+kangaroo(x1, v1, x2, v2)
