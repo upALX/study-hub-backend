8df4f627e1fa682c2c08518172cd3b265fbecc85
Alexandre <alexandreaddres@gmail.com>
2024-01-21 04:35:48 -0300

    typescript folder

--- Hacker_Rank/solved-problems-algo/implementation/klavi_case_challenge.py
+++ Hacker_Rank/solved-problems-algo/implementation/klavi_case_challenge.py
@@ -12,14 +12,23 @@ def get_most_words_appear_by_phrase(phrase: str) -> str:
             counter_dict[word] = 0
         counter_dict[word] += 1
 
-    most_value_appears = max(counter_dict.values())
+    all_values_from_counter_dict_list = [value for value in counter_dict.values()]
+    
+    average_of_values = sum(all_values_from_counter_dict_list) / len(all_values_from_counter_dict_list)
 
-    more_words_appear_list = [word for word, sum_appears in counter_dict.items() if sum_appears == most_value_appears]    
+    print(f'The average of appears is: {average_of_values:.2f}')
 
-    if len(more_words_appear_list) == 1: 
-        return f'The word {more_words_appear_list[0]} appears {most_value_appears} times in the input text'
-    else:
-        return f'Both the words {' and '.join(more_words_appear_list)} appear {most_value_appears} times in the input text'
+    # most_value_appears = sum(counter_dict.values())
+
+    # print(most_value_appears)
+
+    # more_words_appear_list = [word for word, sum_appears in counter_dict.items() if sum_appears == most_value_appears]    
+
+
+    # if len(more_words_appear_list) == 1: 
+    #     return f'The word {more_words_appear_list[0]} appears {most_value_appears} times in the input text'
+    # else:
+    #     return f'Both the words {' and '.join(more_words_appear_list)} appear {most_value_appears} times in the input text'
 
 #because a function need return something
 words_counted = get_most_words_appear_by_phrase(phrase=phrase)
