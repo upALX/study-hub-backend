2e55fe8315ff829eaa724c188770a39a5308e576
Alexandre <alexandreaddres@gmail.com>
2024-02-15 07:10:32 -0300

    adding hacker rank 15/02

---
+++ Hacker_Rank/solved-problems-algo/implementation/find_digits.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'findDigits' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def findDigits(n):
+    # broke the n into list
+    list_of_numbers = list(str(n))
+    # print('list of numbers: ', list_of_numbers)
+    
+    counter_value = 0 
+    for number in list_of_numbers:
+        if int(number) == 0:
+            pass
+        else:
+            if n % int(number) == 0:
+                counter_value += 1
+    
+    # print(f'The counter value is {counter_value}')
+    return counter_value
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = findDigits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
