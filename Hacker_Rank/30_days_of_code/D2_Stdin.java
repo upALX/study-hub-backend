f75ef25c76ec2391c43d70682e30d94b9b5f0814
José Alexandre <alexandreaddres@gmail.com>
2024-10-03 11:29:19 -0300

    adding D2

---
+++ Hacker_Rank/30_days_of_code/D2_Stdin.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+        
+        
+        int catchValueInt = Integer.parseInt(scan.next());
+        scan.nextLine();
+        double catchValueDouble = Double.parseDouble(scan.nextLine());
+        String catchValueString = scan.nextLine();
+        
+        
+        System.out.println(i + catchValueInt);
+        System.out.println(d + catchValueDouble);
+        System.out.println(s + catchValueString);
+        
+
+        /* Declare second integer, double, and String variables. */
+
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        
+        /* Print the sum of both integer variables on a new line. */
+
+        /* Print the sum of the double variables on a new line. */
+		
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+
+        scan.close();
+    }
+}
