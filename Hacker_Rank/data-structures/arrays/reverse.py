ed80588afe3dcb5e9cae719799563cdfb14ab5f2
José Alexandre <alexandreaddres@gmail.com>
2024-10-08 23:26:19 -0300

    reverse array solution

---
+++ Hacker_Rank/data-structures/arrays/reverse.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'reverseArray' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def reverseArray(arr):
+
+    new_arr = []
+    for value in range(1, len(arr) + 1):
+        new_arr.append(arr[len(arr) - value])
+    return new_arr
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
