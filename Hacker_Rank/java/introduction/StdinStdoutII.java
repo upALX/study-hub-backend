bc45367b6961819427ace846e6d6562571749a6a
José Alexandre <alexandreaddres@gmail.com>
2024-10-08 22:05:20 -0300

    adding stdin out II solution

---
+++ Hacker_Rank/java/introduction/StdinStdoutII.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        Double j = scan.nextDouble();
+        scan.nextLine();
+        String k = scan.nextLine();
+
+
+        System.out.println("String: " + k);
+        System.out.println("Double: " + j);
+        System.out.println("Int: " + i);
+    }
+}
