f96830548fc6f1acd3d8fab5c51c23ac1c29f5d7
José Alexandre <alexandreaddres@gmail.com>
2024-11-22 16:59:05 -0300

    select where with not claus

--- sql/sql-select-base.sql
+++ sql/sql-select-base.sql
@@ -32,3 +32,5 @@ WHERE CITY LIKE 'A%'
 SELECT DISTINCT CITY FROM STATION WHERE UPPER(RIGHT(CITY, 1)) IN ('A', 'E', 'I', 'O', 'U');
 --Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
 SELECT DISTINCT CITY FROM STATION WHERE UPPER(LEFT(CITY, 1)) IN ('A', 'E', 'I', 'O', 'U') AND UPPER(RIGHT(CITY, 1)) IN ('A', 'E', 'I', 'O', 'U');
+--Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION WHERE UPPER(LEFT(CITY, 1)) NOT IN ('A',  'E', 'I', 'O', 'U');  
