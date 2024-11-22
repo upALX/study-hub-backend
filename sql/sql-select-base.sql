60ba9e3439c65006265ea35e59a893ab82ececc8
José Alexandre <alexandreaddres@gmail.com>
2024-11-22 17:02:28 -0300

    adding selection with not in on last string

--- sql/sql-select-base.sql
+++ sql/sql-select-base.sql
@@ -34,3 +34,5 @@ SELECT DISTINCT CITY FROM STATION WHERE UPPER(RIGHT(CITY, 1)) IN ('A', 'E', 'I',
 SELECT DISTINCT CITY FROM STATION WHERE UPPER(LEFT(CITY, 1)) IN ('A', 'E', 'I', 'O', 'U') AND UPPER(RIGHT(CITY, 1)) IN ('A', 'E', 'I', 'O', 'U');
 --Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
 SELECT DISTINCT CITY FROM STATION WHERE UPPER(LEFT(CITY, 1)) NOT IN ('A',  'E', 'I', 'O', 'U');  
+--Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION WHERE UPPER(RIGHT(CITY, 1)) NOT IN ('A', 'E', 'I', 'O', 'U');
