be95f827a0a31debb2c835d7d5db3cb2b90f434b
José Alexandre <alexandreaddres@gmail.com>
2024-11-22 17:16:05 -0300

    adding double not in select

--- sql/sql-select-base.sql
+++ sql/sql-select-base.sql
@@ -36,3 +36,5 @@ SELECT DISTINCT CITY FROM STATION WHERE UPPER(LEFT(CITY, 1)) IN ('A', 'E', 'I', 
 SELECT DISTINCT CITY FROM STATION WHERE UPPER(LEFT(CITY, 1)) NOT IN ('A',  'E', 'I', 'O', 'U');  
 --Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
 SELECT DISTINCT CITY FROM STATION WHERE UPPER(RIGHT(CITY, 1)) NOT IN ('A', 'E', 'I', 'O', 'U');
+--Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION WHERE UPPER(LEFT(CITY, 1)) NOT IN ('A', 'E', 'I', 'O', 'U') OR UPPER(RIGHT(CITY, 1)) NOT IN ('A', 'E', 'I', 'O', 'U');
