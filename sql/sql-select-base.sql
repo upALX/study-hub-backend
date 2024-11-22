25a5b693a869332f1eb255423004790502181649
José Alexandre <alexandreaddres@gmail.com>
2024-11-22 16:45:31 -0300

    adding select right left where

--- sql/sql-select-base.sql
+++ sql/sql-select-base.sql
@@ -28,3 +28,5 @@ WHERE CITY LIKE 'A%'
    OR CITY LIKE 'I%' 
    OR CITY LIKE 'O%' 
    OR CITY LIKE 'U%';
+--Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION WHERE UPPER(RIGHT(CITY, 1)) IN ('A', 'E', 'I', 'O', 'U');
