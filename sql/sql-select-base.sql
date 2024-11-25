8bd7ba47bda0a2851726a894361d5717679d4730
José Alexandre <alexandreaddres@gmail.com>
2024-11-25 15:09:53 -0300

    adding order by selection

--- sql/sql-select-base.sql
+++ sql/sql-select-base.sql
@@ -40,3 +40,5 @@ SELECT DISTINCT CITY FROM STATION WHERE UPPER(RIGHT(CITY, 1)) NOT IN ('A', 'E', 
 SELECT DISTINCT CITY FROM STATION WHERE UPPER(LEFT(CITY, 1)) NOT IN ('A', 'E', 'I', 'O', 'U') OR UPPER(RIGHT(CITY, 1)) NOT IN ('A', 'E', 'I', 'O', 'U');
 --Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
 SELECT DISTINCT CITY FROM STATION WHERE UPPER(LEFT(CITY, 1)) NOT IN ('A', 'E', 'I', 'O', 'U') AND UPPER(RIGHT(CITY, 1)) NOT IN ('A', 'E', 'I', 'O', 'U');
+--Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+SELECT name FROM EMPLOYEE order by name ASC;
