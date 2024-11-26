ea86b8b71f7abe4c3c4cf89b1ad7f8b2987e4ee4
José Alexandre <alexandreaddres@gmail.com>
2024-11-26 17:43:18 -0300

    addin count aggregation solution

--- sql/aggregation.sql
+++ sql/aggregation.sql
@@ -1,2 +1,4 @@
 --Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than 1372345 Truncate your answer to  decimal places.
 SELECT ROUND(MAX(LAT_N), 4) FROM STATION WHERE LAT_N < 137.2345;
+--Query a count of the number of cities in CITY having a Population larger than 100000.
+SELECT COUNT(*) FROM CITY WHERE POPULATION > 100000;
