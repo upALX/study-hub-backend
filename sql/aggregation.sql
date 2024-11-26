73a3ca606d650903cf934582443bd7b1d790d274
José Alexandre <alexandreaddres@gmail.com>
2024-11-26 17:46:24 -0300

    adding aggregation using sum

--- sql/aggregation.sql
+++ sql/aggregation.sql
@@ -2,3 +2,5 @@
 SELECT ROUND(MAX(LAT_N), 4) FROM STATION WHERE LAT_N < 137.2345;
 --Query a count of the number of cities in CITY having a Population larger than 100000.
 SELECT COUNT(*) FROM CITY WHERE POPULATION > 100000;
+--Query the total population of all cities in CITY where District is California.
+SELECT SUM(POPULATION) FROM CITY WHERE DISTRICT = 'California';
