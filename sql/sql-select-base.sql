8ed674f90d834cf7661b4197698d54ca9179de88
José Alexandre <alexandreaddres@gmail.com>
2024-11-26 14:27:28 -0300

    adding select with minimal complex where clause

--- sql/sql-select-base.sql
+++ sql/sql-select-base.sql
@@ -44,3 +44,5 @@ SELECT DISTINCT CITY FROM STATION WHERE UPPER(LEFT(CITY, 1)) NOT IN ('A', 'E', '
 SELECT name FROM EMPLOYEE order by name ASC;
 --Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than  per month who have been employees for less than  months. Sort your result by ascending employee_id.
 SELECT name FROM EMPLOYEE WHERE SALARY > 2000 AND MONTHS < 10 ORDER BY EMPLOYEE_ID ASC;
+--Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+SELECT name FROM STUDENTS WHERE MARKS > 75 ORDER BY UPPER(RIGHT(NAME, 3)) ASC, ID ASC; 
