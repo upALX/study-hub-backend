eafb63a0ab4280cdc17a6c1e5b7763f710ee4c31
José Alexandre <alexandreaddres@gmail.com>
2024-11-26 18:01:16 -0300

    adding aggregation using round with average

--- sql/aggregation.sql
+++ sql/aggregation.sql
@@ -6,3 +6,5 @@ SELECT COUNT(*) FROM CITY WHERE POPULATION > 100000;
 SELECT SUM(POPULATION) FROM CITY WHERE DISTRICT = 'California';
 --Query the average population of all cities in CITY where District is California.
 SELECT AVG(POPULATION) FROM CITY WHERE DISTRICT = 'California';
+--Query the average population for all cities in CITY, rounded down to the nearest integer.
+SELECT ROUND(AVG(POPULATION)) FROM CITY;
