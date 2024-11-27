a9d85af17d3920b3a4bbc8aca0352530255ef8bb
José Alexandre <alexandreaddres@gmail.com>
2024-11-27 14:37:16 -0300

    adding aggregation round min

--- sql/aggregation.sql
+++ sql/aggregation.sql
@@ -20,3 +20,5 @@ SELECT ROUND(SUM(LAT_N), 2), ROUND(SUM(LONG_W), 2) FROM STATION;
 SELECT ROUND(SUM(LAT_N), 4) FROM STATION WHERE LAT_N > 38.7880 AND LAT_N < 137.2345;  
 --Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137,2345. Round your answer to 4 decimal places.
 SELECT ROUND(MAX(LONG_W), 4) FROM STATION WHERE LAT_N = (SELECT MAX(LAT_N) FROM STATION WHERE LAT_N < 137.2345);
+--Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 38.7780. Round your answer to  decimal places.
+SELECT ROUND(MIN(LAT_N), 4) FROM STATION WHERE LAT_N > 38.7780; 
