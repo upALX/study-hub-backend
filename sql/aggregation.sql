152eac0a8deacb9573a2b0aaf4049d483cd8164e
José Alexandre <alexandreaddres@gmail.com>
2024-11-27 11:27:47 -0300

    adding aggregation with select sum rounded + wehre clause use

--- sql/aggregation.sql
+++ sql/aggregation.sql
@@ -16,3 +16,5 @@ SELECT MAX(POPULATION) - MIN(POPULATION)  FROM CITY;
 --The sum of all values in LAT_N rounded to a scale of  decimal places.
 --The sum of all values in LONG_W rounded to a scale of  decimal places.
 SELECT ROUND(SUM(LAT_N), 2), ROUND(SUM(LONG_W), 2) FROM STATION;
+--Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7889 and less than 137.2345. Truncate your answer to  decimal places.
+SELECT ROUND(SUM(LAT_N), 4) FROM STATION WHERE LAT_N > 38.7880 AND LAT_N < 137.2345;  
