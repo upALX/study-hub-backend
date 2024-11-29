bb4ee5de7dc645a7c5db121d3e5f344b63126443
José Alexandre <alexandreaddres@gmail.com>
2024-11-29 16:04:17 -0300

    removing and moving insertions + renames folder

--- sql/joins.sql
+++ sql/joins.sql
@@ -1,3 +1,6 @@
 -- Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
 -- Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
 SELECT c.NAME FROM CITY c LEFT JOIN COUNTRY co on co.Code = c.COUNTRYCODE WHERE co.CONTINENT = 'Africa';
+-- Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
+-- Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+SELECT co.CONTINENT, ROUND
