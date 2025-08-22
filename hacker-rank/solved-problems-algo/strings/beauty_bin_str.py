0faf057f6b86c37b08334b1c91ede28f3af72335
José Alexandre <59508016+upALX@users.noreply.github.com>
2025-08-21 22:45:06 -0300

    Adding beauty bin str solved

---
+++ hacker-rank/solved-problems-algo/strings/beauty_bin_str.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'beautifulBinaryString' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING b as parameter.
+#
+
+def beautifulBinaryString(b):
+    count = 0
+    i = 0
+    while i < len(b) - 2:
+        if b[i:i+3] == "010":
+            count += 1
+            i += 3  
+        else:
+            i += 1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    b = input()
+
+    result = beautifulBinaryString(b)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
