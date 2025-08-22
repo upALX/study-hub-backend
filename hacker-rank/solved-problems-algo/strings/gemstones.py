f2218ec4118e30346b65f01d635283ec7c618896
José Alexandre <59508016+upALX@users.noreply.github.com>
2025-08-21 22:41:00 -0300

    Adding gemstones solved

---
+++ hacker-rank/solved-problems-algo/strings/gemstones.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gemstones' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING_ARRAY arr as parameter.
+#
+
+def gemstones(arr):
+    sets = [set(rock) for rock in arr]
+    
+    common = set.intersection(*sets)
+    
+    return len(common)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr_item = input()
+        arr.append(arr_item)
+
+    result = gemstones(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
