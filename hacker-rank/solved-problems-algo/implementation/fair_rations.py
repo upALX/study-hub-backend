9f504c2fb9b62a35c21f7158d4e1e16d76884f04
José Alexandre <59508016+upALX@users.noreply.github.com>
2025-08-31 18:44:27 -0300

    Adding fair rations solved

---
+++ hacker-rank/solved-problems-algo/implementation/fair_rations.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'fairRations' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER_ARRAY B as parameter.
+#
+
+def fairRations(B):
+    odd_count = sum(b % 2 for b in B)
+    if odd_count % 2 != 0:
+        return "NO"
+    loaves = 0
+    for i in range(len(B) - 1):
+        if B[i] % 2 != 0:
+            B[i] += 1
+            B[i+1] += 1
+            loaves += 2
+    return str(loaves)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    N = int(input().strip())
+
+    B = list(map(int, input().rstrip().split()))
+
+    result = fairRations(B)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
---
+++ hacker-rank/solved-problems-algo/strings/mars.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'marsExploration' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def marsExploration(s):
+    changes = 0
+    for i, ch in enumerate(s):
+        if ch != "SOS"[i % 3]:
+            changes += 1
+    return changes
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = marsExploration(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
