4a81e9007161f365e66f9fee838aadcbbd8eaf55
José Alexandre <59508016+upALX@users.noreply.github.com>
2025-09-04 01:41:19 -0300

    Adding operators math

---
+++ java/stdy/operations_math/operations.java
@@ -0,0 +1,12 @@
+package operations_math;
+
+public class operations {
+    public static void main(String[] args) {
+        Integer n = 5;
+        String sum = "1" + 1 + 1 + 1;
+        Double sumRealValues =  Integer.parseInt("1") + 2.0;
+        Double multi = 2.0 * 2;
+
+
+    }
+}
