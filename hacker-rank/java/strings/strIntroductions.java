a0d4f76c429780805f12b69a985043f137e3ccf5
José Alexandre <59508016+upALX@users.noreply.github.com>
2025-09-16 19:40:35 -0300

    adding str introduction solved

---
+++ hacker-rank/java/strings/strIntroductions.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        
+        System.out.println(A.length() + B.length());
+        boolean lexi = A.compareTo(B) > 0 ? true  : false;
+        System.out.println(lexi ? "Yes" : "No"); 
+        System.out.print(A.substring(0, 1).toUpperCase() + A.substring(1).toLowerCase() + " " + B.substring(0, 1).toUpperCase() + B.substring(1).toLowerCase()); 
+        
+    }
+}
+
+
+
